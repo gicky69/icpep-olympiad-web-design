7c8d80dd44efe5aefb112916f305315bc6dd5d19
Carl <carlandrewv@yahoo.com>
2026-03-25 11:42:55 +0800

    Setup Commit

--- my-app/src/main/App.jsx
+++ my-app/src/main/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '../assets/vite.svg'
+import heroImg from '../assets/hero.png'
 import './App.css'
+import LandingPage from '../pages/LandingPage.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
