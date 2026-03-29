a5aa53592247d1908b5a32df0a0f03610f1096c8
Joaquin Pacete <jrpacete@student.apc.edu.ph>
2026-03-29 12:09:01 +0800

    Add header and floating nav
    Add HeaderNavbar and FloatingNavbar components with CSS. Utilized useEffect() for handling user scroll to toggle the floating nav.

--- my-app/src/main/App.jsx
+++ my-app/src/main/App.jsx
@@ -1,16 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Header from '../pages/Header.jsx'
 import Explain from '../pages/Explain-section.jsx'
 import HardwareSoftware from '../pages/HardwareSoftware-section.jsx'
 import Projects from '../pages/Projects-section.jsx'
 import Careers from '../pages/Careers-section.jsx' 
+import HeaderNavbar from '../pages/Navbar.jsx'
+import FloatingNavbar from '../pages/Floating-Navbar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [showFloating, setShowFloating] = useState(false)
+
+  // for floating navbar toggle
+  useEffect(() => {
+    
+    const handleScroll = () => {
+      const scroll = window.scrollY
+
+      setShowFloating(prev => {
+        if (scroll > 800 && !prev) return true
+        if (scroll <= 800 && prev) return false
+        return prev
+      })
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
 
   return (
     <>
+    <HeaderNavbar />
+    <FloatingNavbar show={showFloating}/>
+    
     <section className="header-container">
       <Header/>
     </section>
